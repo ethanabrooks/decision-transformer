5245ccde7cda3dc0b17555369b2e6de09553d775
Ethan Brooks <ethanabrooks@gmail.com>
2022-06-25 14:06:58 -0400

    got torch working without cuda

---
+++ dt/main.py
@@ -0,0 +1,23 @@
+from decision_transformer_atari import GPTConfig, GPT
+import torch
+
+vocab_size = 4
+block_size = 90
+model_type = "reward_conditioned"
+timesteps = 2654
+
+mconf = GPTConfig(
+    vocab_size,
+    block_size,
+    n_layer=6,
+    n_head=8,
+    n_embd=128,
+    model_type=model_type,
+    max_timestep=timesteps,
+)
+model = GPT(mconf)
+
+checkpoint_path = "checkpoints/Breakout_123.pth"  # or Pong, Qbert, Seaquest
+checkpoint = torch.load(checkpoint_path, torch.device('cpu'))
+model.load_state_dict(checkpoint)
+#! /usr/bin/env python
